231f3fd2718bef200fdd65b9095cc4dfd24b1b97
gbrbreen1 <gbrbreen2@gmail.com>
2021-11-04 16:44:22 +0400

    streamlit analytics commit

--- DubaiTrafficPrediction.py
+++ DubaiTrafficPrediction.py
@@ -213,34 +213,34 @@ with open("style.css") as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 
-# footer="""<style>
-# a:link , a:visited{
-# color: blue;
-# background-color: transparent;
-# text-decoration: underline;
-# }
-#
-# a:hover,  a:active {
-# color: red;
-# background-color: transparent;
-# text-decoration: underline;
-# }
-#
-# .footer {
-# position: fixed;
-# left: 0;
-# bottom: 0;
-# width: 100%;
-# background-color: white;
-# color: black;
-# text-align: center;
-# }
-# </style>
-# <div class="footer">
-# <p>&copy; <a href="https://golytics.github.io/" target="_blank">GoLytics</a><br>Developed By: <a href="https://golytics.github.io/" target="_blank">Dr. Mohamed Gabr</a></p>
-# </div>
-# """
-# st.markdown(footer,unsafe_allow_html=True)
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed By: <a href="https://golytics.github.io/" target="_blank">Dr. Mohamed Gabr</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
 
 
 
